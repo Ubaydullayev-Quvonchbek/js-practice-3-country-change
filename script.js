66b9ee15a51bbb1d69a33b6e0b931aa532ae3f6e
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-01-15 15:05:53 +0500

    Team Quvonchbek

---
+++ script.js
@@ -0,0 +1,48 @@
+function uz() {
+    document.getElementById('country1').style.display = "block";
+    document.getElementById('country2').style.display = "none";
+    document.getElementById('country3').style.display = "none";
+    document.getElementById('country4').style.display = "none";
+    document.getElementById('country5').style.display = "none";
+    document.getElementById('country6').style.display = "none";
+}
+function ru() {
+    document.getElementById('country1').style.display = "none";
+    document.getElementById('country2').style.display = "block";
+    document.getElementById('country3').style.display = "none";
+    document.getElementById('country4').style.display = "none";
+    document.getElementById('country5').style.display = "none";
+    document.getElementById('country6').style.display = "none";
+}
+function fr() {
+    document.getElementById('country1').style.display = "none";
+    document.getElementById('country2').style.display = "none";
+    document.getElementById('country3').style.display = "flex";
+    document.getElementById('country4').style.display = "none";
+    document.getElementById('country5').style.display = "none";
+    document.getElementById('country6').style.display = "none";
+} 
+function it() {
+    document.getElementById('country1').style.display = "none";
+    document.getElementById('country2').style.display = "none";
+    document.getElementById('country3').style.display = "none";
+    document.getElementById('country4').style.display = "flex";
+    document.getElementById('country5').style.display = "none";
+    document.getElementById('country6').style.display = "none";
+} 
+function ja() {
+    document.getElementById('country1').style.display = "none";
+    document.getElementById('country2').style.display = "none";
+    document.getElementById('country3').style.display = "none";
+    document.getElementById('country4').style.display = "none";
+    document.getElementById('country5').style.display = "block";
+    document.getElementById('country6').style.display = "none";
+} 
+function gr() {
+    document.getElementById('country1').style.display = "none";
+    document.getElementById('country2').style.display = "none";
+    document.getElementById('country3').style.display = "none";
+    document.getElementById('country4').style.display = "none";
+    document.getElementById('country5').style.display = "none";
+    document.getElementById('country6').style.display = "block";
+}
